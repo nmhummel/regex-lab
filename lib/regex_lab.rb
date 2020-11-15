cab2548a8ecd6c9e8accfbc459df66111aded8be
nmhummel <natalie.hummel@gmail.com>
2020-11-15 12:31:08 -0600

    commit tree dfeea290863179639207c73807d5a6b2eb009aa9

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+    /[aeiou]/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    /^[un]||$[ing]/
 end
 
 def words_five_letters_long(text)
-
+    /\w{5}\s/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
